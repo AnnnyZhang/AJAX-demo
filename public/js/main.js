4145a79fc94f4b96225b9cbc17b12ce1ab82ff0e
AmeyZhang <18175008653@163.com>
2020-07-12 09:54:54 +0800

    调整文件加载路径

--- public/js/main.js
+++ public/js/main.js
@@ -56,7 +56,7 @@ getHTML.onclick = ()=>{
 
 getXML.onclick = ()=>{
     const request = new XMLHttpRequest();
-    request.open('GET', '/demo.xml');
+    request.open('GET', 'demo.xml');
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             const status = request.status;
@@ -73,7 +73,7 @@ getXML.onclick = ()=>{
 
 getJSON.onclick = ()=>{
     const request = new XMLHttpRequest();
-    request.open('GET', '../db/page1.json');
+    request.open('GET', '../../db/page1.json');
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             const {status} = request;
@@ -90,7 +90,7 @@ getJSON.onclick = ()=>{
 let i = 1;
 getPAGE.onclick = ()=>{
     const request = new XMLHttpRequest();
-    request.open('GET', `../db/page${++i}.json`);
+    request.open('GET', `../../db/page${++i}.json`);
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             const {status} = request;
